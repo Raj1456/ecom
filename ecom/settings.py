07f92f5fa0f2e3f9b587af0912e2237312cfa828
Raju <rajagopalraju6@gmail.com>
2021-05-11 06:26:06 +0530

    store and carts functionality

--- ecom/settings.py
+++ ecom/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
+
             ],
         },
     },
