1938912d12cf272f74648e5839e6723b7c8bce01
Raju <rajagopalraju6@gmail.com>
2021-05-13 07:54:08 +0530

    authentication

--- ecom/settings.py
+++ ecom/settings.py
@@ -131,3 +131,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 
 }
+
+#SMTP configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='rrrajagopal9@gmail.com'
+EMAIL_HOST_PASSWORD='9747183214'
+EMAIL_USE_TLS=True
